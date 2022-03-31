c37e4a68e6d9fb81c7479363e2519800421cc640
w4dd325 <w4dd325@gmail.com>
2022-03-31 17:18:57 +0100

    Completed Section

---
+++ Learn-JavaScript/39-Functions-Function-Expressions.js
@@ -0,0 +1,9 @@
+const plantNeedsWater = function plantNeedsWater(day){
+    if (day === 'Wednesday'){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  console.log(plantNeedsWater('Tuesday'));
+  console.log(plantNeedsWater('Wednesday'));
