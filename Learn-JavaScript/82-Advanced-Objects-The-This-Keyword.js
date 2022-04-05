4c167aa340769b5c37c5dce4fe6eae0741594ed6
w4dd325 <w4dd325@gmail.com>
2022-04-05 12:15:29 +0100

    Completed Section

--- Learn-JavaScript/82-Advanced-Objects-The-This-Keyword.js
+++ Learn-JavaScript/82-Advanced-Objects-The-This-Keyword.js
@@ -3,5 +3,6 @@ const robot = {
     energyLevel:100,
     provideInfo (){
       return `I am ${this.model} and my current energy level is ${this.energyLevel}.`}
-  };
+};
+
   console.log(robot.provideInfo())
