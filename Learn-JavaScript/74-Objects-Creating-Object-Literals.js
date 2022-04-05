4901addcf42a15e615ba52d347301b80fa02254b
w4dd325 <w4dd325@gmail.com>
2022-04-05 11:44:08 +0100

    Completed Section

---
+++ Learn-JavaScript/74-Objects-Creating-Object-Literals.js
@@ -0,0 +1,11 @@
+// Write your fasterShip object literal below
+
+let spaceship = {
+    'Fuel Type': 'diesel',
+    color: 'silver'
+  };
+  
+  let fasterShip = {
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel'
+  };
